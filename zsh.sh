dba1a5af33031b11e47607600d228f3eb747a0fa
stilwellj1983 <stilwellj1983@users.noreply.github.com>
2020-10-14 15:56:33 -0400

    Update zsh.sh
    added auto confirm

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo apt update && sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions
+sudo apt update && sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions -y
 cp .zshrc ~/
 chsh -s $(which zsh)
 zsh
